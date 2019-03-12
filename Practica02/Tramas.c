88cbbd5d270ec495805c7143252dfa13b272a511
asdf1234Damian <asdf1234damian@gmail.com>
2019-03-11 20:11:24 -0600

    Practica 2, segunda entrega

--- Practica02/Tramas.c
+++ Practica02/Tramas.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
-unsigned char t[]={0x48,0xc6,0xCF,0xBB,0x48,0x48,0x64,0x21};
-unsigned char  g[][5]={"UTP", "OPTI", "COAX"};
+unsigned char t[]={0x05,0xc6,0xCF,0xdb,0x48,0x88,0x69,0x21};
+unsigned char g[][5]={"UTP", "OPTI", "COAX"};
 unsigned char ng[][5]={" ", "BT", "WF", "ST"};
 unsigned char gr[][5]={" ", "3CM2", "4CM1", "2CM6"};
 unsigned char lg[][5]={"JPN", "ENG", "FRA", "ESP"};
-int main(int argc, char const *argv[]) {
+
+unsigned char bitsPrendidos(char *trama){
+  unsigned char  res = 0;
+  for (unsigned char i = 0; i < sizeof(trama); i++) {
+    for (unsigned char j = 128; j > 0 ; j=j>>1) {
+      if(trama[i]&j){res++;}
+    }
+  }
+  return res;
+}
+
+int checksum(char *trama){
+int sum = 0;
+  for (unsigned char i = 0; i < sizeof(trama); i+=2) {
+    sum += trama[i]<<8 + trama[i+1];
+  }
+  return sum;
+}
+
+
+void analizarTrama(char * t) {
   printf("Codigo de linea: ");
   if (t[0]&128)
     printf("Unipolar\n");
@@ -36,13 +56,16 @@ int main(int argc, char const *argv[]) {
   printf("Control de error:");
   switch ((t[1]>>2)&3) {
     case 0:
-    printf(" \n");
+    printf("Bit de paridad par\n");
+    (bitsPrendidos(t)%2)?printf(":(\n"):printf(":)\n");
     break;
     case 1:
     printf("CheckSum\n");
+    printf("%x\n", checksum(t) );
     break;
     case 2:
-    printf("Bit de paridad\n");
+    printf("Bit de paridad impar\n");
+    (bitsPrendidos(t)%2)?printf(":)\n"):printf(":(\n");
     break;
     case 3:
     printf("CRC\n");
@@ -61,12 +84,18 @@ int main(int argc, char const *argv[]) {
   printf("Idioma: %s\n", lg[t[4]>>6&3] );
 
 
-  printf("Tamaño: %d byte\n",(t[4]>>3&5));
+  printf("Tamaño: %d byte(s)\n",((t[4]>>3&5)*3));
 
   if (t[4]>>2&1) {
-    printf("Fragmentado\n Byte # : %d ",(t[4]&3));
+    printf("Fragmentado\n # Seq : %d ",(t[4]&3));
   } else {
     printf("No Fragmentado\n");
   }
-  return 0;
+}
+
+
+int main(int argc, char const *argv[]) {
+  for (size_t i = 0; i < 1; i++) {
+    analizarTrama(t);
+  }
 }
